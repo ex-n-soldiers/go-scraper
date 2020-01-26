60612c71b2def56e861f0ee01f5ab15b26090472
mongolyy <mongolyy@gmail.com>
2020-01-26 19:54:23 +0900

    エラーハンドリングの方法を見直し

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/spf13/viper"
 	"github.com/t-tiger/gorm-bulk-insert"
 	"io"
-	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -22,10 +21,16 @@ var currentDirectory, _ = os.Getwd()
 var downloadBasePath = filepath.Join(currentDirectory, "work", "downloadFiles")
 
 func main() {
-	config := configure()
+	config, err := configure()
+	if err != nil {
+		panic(err)
+	}
 
-	db := gormConnect(config)
+	db, err := gormConnect(config)
 	defer db.Close()
+	if err != nil {
+		panic(err)
+	}
 
 	body, err := getBody(baseURL)
 	if err != nil {
@@ -37,24 +42,21 @@ func main() {
 		panic(err)
 	}
 
-	err = registerCurrentData(items, db)
-	if err != nil {
+	if err := registerCurrentData(items, db); err != nil {
 		panic(err)
 	}
 
-	err = updateItemMaster(db)
-	if err != nil {
+	if err := updateItemMaster(db); err != nil {
 		panic(err)
 	}
 
 	durationDays := 5
-	err = fetchDetailPages(db, durationDays)
-	if err != nil {
+	if err := fetchDetailPages(db, durationDays); err != nil {
 		panic(err)
 	}
 }
 
-func gormConnect(config Config) *gorm.DB {
+func gormConnect(config Config) (*gorm.DB, error) {
 	var dbHost = config.Db.Host
 	var dbPort = config.Db.Port
 	var dbName = config.Db.DbName
@@ -63,19 +65,21 @@ func gormConnect(config Config) *gorm.DB {
 
 	db, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName))
 	if err != nil {
-		panic(err.Error())
+		return nil, fmt.Errorf("DB connection error: %w", err)
 	}
 
-	db.AutoMigrate(&ItemMaster{}, &LatestItem{})
-	return db
+	if err := db.AutoMigrate(&ItemMaster{}, &LatestItem{}).Error; err != nil {
+		return nil, fmt.Errorf("DB migration error: %w", err)
+	}
+	return db, nil
 }
 
 func getBody(url string) (io.ReadCloser, error) {
 	res, err := http.Get(url)
 	if err != nil {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		return nil, fmt.Errorf("HTTP Get request error: %v %s %w", res.StatusCode, res.Status, err)
 	}
-	return res.Body, err
+	return res.Body, nil
 }
 
 func getList(body io.ReadCloser) ([]Item, error) {
@@ -83,15 +87,17 @@ func getList(body io.ReadCloser) ([]Item, error) {
 
 	doc, err := goquery.NewDocumentFromReader(body)
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("Get document error: %w", err)
 	}
 
 	doc.Find("table tr").Each(func(_ int, s *goquery.Selection) {
 		item := Item{}
 		item.Name = s.Find("td:nth-of-type(2) a").Text()
 		item.Price, _ = strconv.Atoi(strings.ReplaceAll(strings.ReplaceAll(s.Find("td:nth-of-type(3)").Text(), ",", ""), "円", ""))
-		uri, _ := s.Find("td:nth-of-type(2) a").Attr("href")
-		item.Url = baseURL + uri
+		uri, exists := s.Find("td:nth-of-type(2) a").Attr("href")
+		if exists {
+			item.Url = baseURL + uri
+		}
 		if item.Name != "" {
 			items = append(items, item)
 		}
@@ -100,70 +106,72 @@ func getList(body io.ReadCloser) ([]Item, error) {
 }
 
 func registerCurrentData(items []Item, db *gorm.DB) error {
-	db.Exec("TRUNCATE " + db.NewScope(&LatestItem{}).TableName())
+	if err := db.Exec("TRUNCATE " + db.NewScope(&LatestItem{}).TableName()).Error; err != nil {
+		return fmt.Errorf("Truncate table error: %w", err)
+	}
 
 	var insertRecords []interface{}
 	for _, item := range items {
 		insertRecords = append(insertRecords, LatestItem{Item: item})
 	}
-	err := gormbulk.BulkInsert(db, insertRecords, 2000)
-	return err
+	if err := gormbulk.BulkInsert(db, insertRecords, 2000); err != nil {
+		return fmt.Errorf("Bulk insert error: %w", err)
+	}
+	return nil
 }
 
 func updateItemMaster(db *gorm.DB) error {
 	// Insert
 	var newItems []LatestItem
-	err := db.Unscoped().Joins("left join item_master on latest_items.url = item_master.url").Where("item_master.name is null").Find(&newItems).Error
-	if err != nil {
-		return err
+	if err := db.Unscoped().Joins("left join item_master on latest_items.url = item_master.url").Where("item_master.name is null").Find(&newItems).Error; err != nil {
+		return fmt.Errorf("Insert error: %w", err)
 	}
 
 	var insertRecords []interface{}
 	for _, newItem := range newItems {
 		insertRecords = append(insertRecords, ItemMaster{Item: newItem.Item})
 	}
-	err = gormbulk.BulkInsert(db, insertRecords, 2000)
-	if err != nil {
-		return err
+	if err := gormbulk.BulkInsert(db, insertRecords, 2000); err != nil {
+		return fmt.Errorf("Bulk insert error: %w", err)
 	}
 
 	// Update
 	var updatedItems []LatestItem
-	err = db.Unscoped().Joins("inner join item_master on latest_items.url = item_master.url").Where("latest_items.name <> item_master.name or latest_items.price <> item_master.price or item_master.deleted_at is not null").Find(&updatedItems).Error
-	if err != nil {
-		return err
+	if err := db.Unscoped().Joins("inner join item_master on latest_items.url = item_master.url").Where("latest_items.name <> item_master.name or latest_items.price <> item_master.price or item_master.deleted_at is not null").Find(&updatedItems).Error; err != nil {
+		return fmt.Errorf("Update error: %w", err)
 	}
 	for _, updatedItem := range updatedItems {
-		err := db.Unscoped().Model(ItemMaster{}).Where("url = ?", updatedItem.Url).Updates(map[string]interface{}{"nam": updatedItem.Name, "price": updatedItem.Price, "deleted_at": nil}).Error
-		if err != nil {
-			return err
+		if err := db.Unscoped().Model(ItemMaster{}).Where("url = ?", updatedItem.Url).Updates(map[string]interface{}{"nam": updatedItem.Name, "price": updatedItem.Price, "deleted_at": nil}).Error; err != nil {
+			return fmt.Errorf("Update error: %w", err)
 		}
 	}
 
 	// Delete
-	err = db.Where("not exists(select 1 from latest_items li where li.url = item_master.url)").Delete(&ItemMaster{}).Error
-	return err
+	if err := db.Where("not exists(select 1 from latest_items li where li.url = item_master.url)").Delete(&ItemMaster{}).Error; err != nil {
+		return fmt.Errorf("Delete error: %w", err)
+	}
+
+	return nil
 }
 
 func fetchDetailPages(db *gorm.DB, durationDays int) error {
 	var items []ItemMaster
-	err := db.Where("last_checked_at < ?", time.Now().AddDate(0, 0, -durationDays)).Find(&items).Error
-	if err != nil {
-		return err
+	if err := db.Where("last_checked_at < ?", time.Now().AddDate(0, 0, -durationDays)).Find(&items).Error; err != nil {
+		return fmt.Errorf("Select error: %w", err)
 	}
 
 	for _, item := range items {
 		body, err := getBody(item.Url)
 		if err != nil {
-			return err
+			return fmt.Errorf("Fetch detail page body error: %w", err)
 		}
 
 		itemWithDetails, err := getDetails(body, item)
 		if err != nil {
-			return err
+			return fmt.Errorf("Fetch detail page content error: %w", err)
 		}
 
-		err = db.Model(&itemWithDetails).Updates(ItemMaster{
+		if err = db.Model(&itemWithDetails).Updates(ItemMaster{
 			Description: itemWithDetails.Description,
 			LastCheckedAt: time.Now(),
 			ImageUrl: itemWithDetails.ImageUrl,
@@ -171,9 +179,8 @@ func fetchDetailPages(db *gorm.DB, durationDays int) error {
 			ImageDownloadPath: itemWithDetails.ImageDownloadPath,
 			PDFUrl: itemWithDetails.PDFUrl,
 			PDFLastModifiedAt: itemWithDetails.PDFLastModifiedAt,
-			PDFDownloadPath: itemWithDetails.PDFDownloadPath}).Error
-		if err != nil {
-			return err
+			PDFDownloadPath: itemWithDetails.PDFDownloadPath}).Error; err != nil {
+			return fmt.Errorf("Update item detail info error: %w", err)
 		}
 	}
 	return nil
@@ -182,7 +189,7 @@ func fetchDetailPages(db *gorm.DB, durationDays int) error {
 func getDetails(body io.ReadCloser, item ItemMaster) (ItemMaster, error) {
 	doc, err := goquery.NewDocumentFromReader(body)
 	if err != nil {
-		log.Fatal(err)
+		return ItemMaster{}, fmt.Errorf("Get detail page document body error %w", err)
 	}
 
 	item.Description = doc.Find("table tr:nth-of-type(2) td:nth-of-type(2)").Text()
@@ -196,7 +203,7 @@ func getDetails(body io.ReadCloser, item ItemMaster) (ItemMaster, error) {
 		item.ImageLastModifiedAt = currentLastModified
 		imageDownloadPath, err := downloadFile(imageUrl, filepath.Join(downloadBasePath, "img", strconv.Itoa(int(item.ID)), item.ImageFileName()))
 		if err != nil {
-			return item, err
+			return ItemMaster{}, fmt.Errorf("Download image error: %w", err)
 		}
 		item.ImageDownloadPath = imageDownloadPath
 	}
@@ -210,12 +217,12 @@ func getDetails(body io.ReadCloser, item ItemMaster) (ItemMaster, error) {
 		item.PDFLastModifiedAt = currentLastModified
 		pdfDownloadPath, err := downloadFile(pdfUrl, filepath.Join(downloadBasePath, "pdf", strconv.Itoa(int(item.ID)), item.PDFFileName()))
 		if err != nil {
-			return item, err
+			return ItemMaster{}, fmt.Errorf("Download pdf error: %w", err)
 		}
 		item.PDFDownloadPath = pdfDownloadPath
 	}
 
-	return item, err
+	return item, nil
 }
 
 func checkFileUpdated(fileUrl string, lastModified time.Time) (isUpdated bool, currentLastModified time.Time) {
@@ -234,45 +241,48 @@ func checkFileUpdated(fileUrl string, lastModified time.Time) (isUpdated bool, c
 func getLastModified(fileUrl string) (time.Time, error) {
 	res, err := http.Head(fileUrl)
 	if err != nil {
-		return time.Unix(0, 0), err
+		return time.Time{}, fmt.Errorf("HTTP HEAD request error: %w", err)
 	}
 	lastModified, err := time.Parse("Mon, 02 Jan 2006 15:04:05 MST", res.Header.Get("Last-Modified"))
-	return lastModified, err
+	if err != nil {
+		return time.Time{}, fmt.Errorf("Get last-modified attribute error: %w", err)
+	}
+	return lastModified, nil
 }
 
 func downloadFile(url string, downloadPath string) (downloadedPath string, err error) {
 	// Create base directory
 	err = os.MkdirAll(filepath.Dir(downloadPath), 0777)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("Mkdir error during download file: %w", err)
 	}
 
 	// Create the file
 	out, err := os.Create(downloadPath)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("Create file error during download file: %w", err)
 	}
 	defer out.Close()
 
 	// Get the data
-	fmt.Println("Download File: " + url)
 	resp, err := http.Get(url)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("Download file error: %w", err)
+	} else {
+		fmt.Println("Download File: ", url)
 	}
 	defer resp.Body.Close()
 
 	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		return "", err
+	if _, err = io.Copy(out, resp.Body); err != nil {
+		return "", fmt.Errorf("Copy file error during download file: %w", err)
 	}
 
 	downloadedPath = downloadPath + filepath.Base(downloadPath)
 	return downloadedPath, nil
 }
 
-func configure() Config {
+func configure() (Config, error) {
 	var config Config
 	viper.SetDefault("db.host", "localhost")
 	viper.SetDefault("db.port", "3306")
@@ -289,14 +299,12 @@ func configure() Config {
 	viper.AddConfigPath(filepath.Join(".", "conf"))
 	viper.AutomaticEnv()
 	if err := viper.ReadInConfig(); err != nil {
-		fmt.Println("Read config file error: ", err)
-		os.Exit(1)
+		return Config{}, fmt.Errorf("Read config file error: %w", err)
 	}
 
 	if err := viper.Unmarshal(&config); err != nil {
-		fmt.Println("Unmarshal config file error: ", err)
-		os.Exit(1)
+		return Config{}, fmt.Errorf("Unmarshal config file error: %w", err)
 	}
 
-	return config
+	return config, nil
 }
